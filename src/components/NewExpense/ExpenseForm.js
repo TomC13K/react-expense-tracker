17a65e165c7b00f27fa6162709e4bc3a6e5b96ef
Tomas Ciernik <Tciernik@outlook.com>
2021-06-11 15:08:23 +0100

    added twi way binding of state values to input boxes and submit function

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,50 +2,68 @@ import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  //const [enteredTitle, setEnteredTitle] = useState("");
-  //const [enteredAmount, setEnteredAmount] = useState("");
-  //const [entereDate, setEnteredDate] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-  const [userInput,setUserInput]=useState({
-    enteredTitle:'',
-    enteredAmount:'',
-    enteredDate:''
-  });
+  // const [userInput,setUserInput]=useState({
+  //   enteredTitle:'',
+  //   enteredAmount:'',
+  //   enteredDate:''
+  // });
 
   const titleChangeHandler = (event) => {
-    //setEnteredTitle(event.target.value);
-    
-    //by using this I am rewritting whole object so need to use spread operator to copy old values, 
+    setEnteredTitle(event.target.value);
+
+    //by using this I am rewritting whole object so need to use spread operator to copy old values,
     //otherwise I overwrite them here....and only set the desired property of object -> enteredTitle
-    setUserInput({
-      ...userInput,
-      enteredTitle:event.target.value,
-    });
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle:event.target.value,
+    // });
     //console.log(event.target.value); //inside the event of this target element is property VALUE
   };
 
   const amountChangeHandler = (event) => {
-    //setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredAmount:event.target.value,
-    });
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount:event.target.value,
+    // });
   };
 
   const dateChangeHandler = (event) => {
-    //setEnteredDate(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredDate:event.target.value,
-    });
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate:event.target.value,
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -53,6 +71,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -62,6 +81,7 @@ const ExpenseForm = () => {
             type="date"
             min="2020-01-01"
             max="2025-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
